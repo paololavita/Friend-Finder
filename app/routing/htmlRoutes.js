de4cfa70129f932d2797a610c76324881d88437d
Jonas Grumby <plavita@hotmail.com>
2018-08-02 11:24:12 -0400

    Second Push

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,40 @@
+// ===============================================================================
+// DEPENDENCIES
+// We need to include the path package to get the correct file path for our html
+// ===============================================================================
+var path = require('path');
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function (app) {
+	// HTML GET Requests
+	// Below code handles when users "visit" a page.
+	// In each of the below cases the user is shown an HTML page of content
+	// ---------------------------------------------------------------------------
+
+	app.get('/', function (req, res) {
+        //res.sendFile(path.join(__dirname + '/../public/survey.html'));
+        res.send("This is the Index Page");
+    });
+    
+	app.get('/survey', function (req, res) {
+        //res.sendFile(path.join(__dirname + '/../public/survey.html'));
+        res.send("This is the Survey Page");
+	});
+    
+    // If no matching route is found default to home
+	app.use(function (req, res) {
+        //res.sendFile(path.join(__dirname + '/../public/home.html'));
+        res.send("This is the 404 Error Page");
+	});
+};
+
+    
+    
+    
+    
+    
+    
---
+++ server.js
@@ -0,0 +1,69 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+const express = require('express')
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+
+const app = express()
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+//require("./app/routing/api-routes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+/*app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/survey', function (req, res) {
+    res.send('survey')
+  })*/
+  
+app.get('/api/friends', function (req, res) {
+    res.json({
+        "name":"Joe Blow",
+        "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores":[
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+          ]
+      })
+})
+  
+  app.post('/api/friends', function (req, res) {
+    res.send('post cereal')
+  })
+
+
+
+// ==============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// ==============================================================================
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
